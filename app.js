bbdbac8bf7ef3e1b6fb5d0ace18b89b1a4d3bfe8
jennyrhee <jennyirhee@gmail.com>
2022-01-21 12:32:13 -0600

    Make calculator functional

--- app.js
+++ app.js
@@ -19,17 +19,51 @@ function operate(operator, a, b) {
 }
 
 const calc = {
+  lastInput: null,
   output: '',
-  total: 0,
+  total: null,
   operator: null,
+  needsNextOperand: false,
   numberButtons: document.querySelectorAll('.number-btn'),
   operatorButtons: document.querySelectorAll('.operator-btn'),
   clearButton: document.getElementById('clear-btn')
 };
 
 calc.numberButtons.forEach(btn => btn.addEventListener('click', () => {
+  // if last input was =, also clear i.e., resets calculator
+  if (calc.needsNextOperand || calc.lastInput === '=') {
+    calc.needsNextOperand = false;
+    clearOutput();
+  }
   calc.output += btn.textContent;
+  calc.lastInput = btn.textContent;
   displayOutput();
 }))
+
+calc.operatorButtons.forEach(btn => btn.addEventListener('click', () => {
+  let operator = btn.textContent;
+  let output = Number(calc.output);
+  clearOutput();
+
+  if (!calc.operator) {
+    calc.total = output;
+    calc.operator = operator;
+  }
+  // if last input was operator (except =), update operator
+  else if (/[^=\d]/.test(calc.lastInput)) calc.operator = operator; 
+  // if lastInput was also =, breaks the operate function
+  else if (operator === '=' && calc.lastInput !== '=') {
+    calc.output = operate(calc.operator, calc.total, output);
+    calc.operator = null;
+  } else if (calc.lastInput !== '=') {
+    calc.output = operate(calc.operator, calc.total, output);
+    calc.total = calc.output;
+    calc.operator = operator;
+    calc.needsNextOperand = true;
+  }
+  displayOutput();
+  calc.lastInput = operator;
+  console.table(calc);
+}));
 
 calc.clearButton.addEventListener('click', clearOutput);
