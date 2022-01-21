8dc7453e83b74a02b56da0b09241c1ee8932561e
jennyrhee <jennyirhee@gmail.com>
2022-01-21 11:09:16 -0600

    Create calc obj and add operate func

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 function displayOutput() {
   let display = document.getElementById('display');
-  display.textContent = output;
+  display.textContent = calc.output;
 }
 
 function clearOutput() {
-  output = '';
+  calc.output = '';
   displayOutput();
 }
 
-let output = '';
+function operate(operator, a, b) {
+  switch (operator) {
+    // hex codes
+    case ('\x2b'): return a + b;
+    case ('\u2212'): return a - b;
+    case ('\xD7'): return a * b;
+    case ('\xF7'): return a / b;
+  }
+}
 
-const buttons = document.querySelectorAll('.number-btn');
-buttons.forEach(btn => btn.addEventListener('click', () => {
-  output += btn.textContent;
+const calc = {
+  output: '',
+  total: 0,
+  operator: null
+};
+
+const numberButtons = document.querySelectorAll('.number-btn');
+numberButtons.forEach(btn => btn.addEventListener('click', () => {
+  calc.output += btn.textContent;
   displayOutput();
 }))
 
